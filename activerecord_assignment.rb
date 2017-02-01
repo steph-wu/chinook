cd21970fd43d8729515f6aa62baa50b34c37122b
STEPHANIE <WU.STEPHANIE@LIVE.COM>
2017-02-01 18:36:19 -0500

    Complete

--- activerecord_assignment.rb
+++ activerecord_assignment.rb
@@ -6,29 +6,31 @@
 
 # 1a) Find the genre with the name "Hip Hop/Rap".
 
-
+Genre.find_by(name: "Hip Hop/Rap")
 
 # 1b) Count how many tracks belong to the "Hip Hop/Rap" genre
 
-
+Track.where(genre_id: 17).count
 
 # 2) Find the total amount of time required to listen to all the tracks in the database.
 
-
+Track.sum(:milliseconds)
 
 # 3a) Find the highest price of any track that has the media type "MPEG audio file".
 
-
+Track.where(media_type_id = 1).maximum(:unit_price)
 
 # 3b) Find the name of the most expensive track that has the media type "MPEG audio file".
 
-
+Track.where(media_type_id = 1).order(unit_price: :desc).first
 
 # 4) Find the 2 oldest artists.
 
-
+Artist.order(created_at: :desc).limit(2)
 
 ### Stretch Exercise (Active Record Query Interface)
 
 
 # 1) Find all the albums whose titles start with B.
+
+Album.where('title LIKE ?', 'B%').all
--- sql_assignment.sql
+++ sql_assignment.sql
@@ -6,18 +6,20 @@
 
 -- 1) Find the albums recorded by the artist Queen.
 
-
+SELECT title FROM albums WHERE artist_id = (SELECT id FROM artists WHERE name = 'Queen');
 
 -- 2) [Count](http://www.w3schools.com/sql/sql_func_count.asp) how many tracks belong to the media type "Protected MPEG-4 video file".
 
-
+SELECT COUNT(name) FROM tracks WHERE media_type_id = (SELECT id FROM media_types WHERE name = 'Protected MPEG-4 video file')
 
 -- 3) Find the least expensive track that has the genre "Electronica/Dance".
 
-
+SELECT MAX(unit_price) FROM tracks WHERE genre_id = (SELECT id FROM genres WHERE name = 'Electronica/Dance');
 
 -- 4) Find the all the artists whose names start with A.
 
-
+SELECT name FROM artists WHERE name LIKE 'A%';
 
 -- 5) Find all the tracks that belong to playlist 1.
+
+SELECT name FROM tracks INNER JOIN playlists_tracks ON tracks.id=playlists_tracks.track_id WHERE playlists_tracks.playlist_id = 1
